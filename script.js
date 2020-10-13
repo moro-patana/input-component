029331c49347ccd360687cd3ebad02ed357eb497
Manjaka Randrianirina <manjaka.ran@onja.org>
2020-10-13 15:00:59 +0300

    Changed the h1 textcontent

--- script.js
+++ script.js
@@ -1,4 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Input from "./inputs.js"
 
-ReactDOM.render(<h1>Hello Onja</h1>, document.getElementById('root'));
+function App() {
+    return (
+        <main>
+            <h1>Inputs</h1>
+                {`<Input />`}
+            <fieldset>
+                <label>Label</label>
+                <Input type="text" placeholder="Placeholder" />
+            </fieldset>
+            {`<Input error />`}
+            <fieldset>
+                <label className="error">Label</label>
+                <Input type="text" placeholder="Placeholder" error="error" />
+            </fieldset>
+            {`<Input disabled />`}
+            <fieldset>
+                <label>Label</label>
+                <Input type="text" disabled placeholder="Placeholder" />
+            </fieldset>
+            {`<Input helperText="some interesting text" />`}
+            <fieldset>
+                <label>Label</label>
+                <Input type="text" helperText="some interesting text" placeholder="Placeholder" />
+                Some interesting text
+            </fieldset>
+
+
+
+        </main>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
