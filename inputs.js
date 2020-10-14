4cae57676594fdf88453bf2bcd9d45ad75c59f75
Manjaka Randrianirina <manjaka.ran@onja.org>
2020-10-14 13:34:08 +0300

    set condition to the startIcon, endIcon and disabled input class name

--- inputs.js
+++ inputs.js
@@ -3,19 +3,37 @@ import React from "react"
 function Input(props) {
     let classNames = props.helperText ? `input input-${props.helperText}` : 'input';
     if (props.error) {
-        classNames = `${classNames} input-${props.error}`
+        classNames = `${classNames} input-error`
     }
 
     if (props.size) {
         classNames = `${classNames} input-${props.size}`
     }
+    if (props.disabled) {
+        classNames = `${classNames} input-disabled`
+    }
+    if (props.startIcon) {
+        classNames = `${classNames} input-startIcon`
+    }
+    if (props.endIcon) {
+        classNames = `${classNames} input-endIcon`
+    }
     if (props.value) {
         classNames = `${classNames} input-${props.value}`
     }
+    if (props.fullwidth) {
+        classNames = `${classNames} input-fullwidth`
+    }
+    if (props.multiline) {
+        classNames = `${classNames} input-multiline`
+    }
+
+
+
     return (
             <input 
             className={classNames} 
-            disabled={props.disabled} 
+            value={props.value}
             placeholder={props.placeholder} />
     )
 
